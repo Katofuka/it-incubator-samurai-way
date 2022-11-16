2b301b2b5577207f08445ba3e34ff9dfffc484ce
Katopuk <ladno4ka@gmail.com>
2022-11-16 08:29:50 +0700

    some changes

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -7,6 +7,8 @@ export const Profile = () => {
                 <img className="ava" alt='avatar'
                      src="https://abrakadabra.fun/uploads/posts/2022-01/1642131987_41-abrakadabra-fun-p-tyanka-na-prozrachnom-fone-48.jpg"/>
                 <p>ava+descriptions</p>
+                <p>some text</p>
+                <p>some text</p>
             </div>
             <div>My posts</div>
             <div>New post</div>
