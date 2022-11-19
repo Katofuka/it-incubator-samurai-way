9573c38524395c82c431f60e10b673b8d7192551
Katopuk <ladno4ka@gmail.com>
2022-11-19 13:26:58 +0700

    add dialogs module

---
+++ src/components/Dialogs/Dialogs.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import style from './Diologs.module.css'
+
+export const Dialogs = () => {
+    return (
+        <div className={style.h1}>
+
+        </div>
+    );
+};
